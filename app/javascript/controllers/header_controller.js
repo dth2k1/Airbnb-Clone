024e23b1b89659936b043ba4df8b6a88430e712b
dth2k1 <gblhiep@gmail.com>
2023-04-09 23:52:46 +0700

    3 add modal and move routes

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,12 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
-import {enter, leave, toggle} from 'el-transition'
+import { enter, leave, toggle } from "el-transition"
+import ModalController from "controllers/modal_controller"
 
 export default class extends Controller {
-  static targets = ["openUserMenu"]
-
+  static targets = ["openUserMenu", "userAuthLink"]
   connect() {
     const btnToggleMenu = this.openUserMenuTarget
+    const btnAuth = this.userAuthLinkTargets
     btnToggleMenu.addEventListener('click', this.toggleDropdownMenu)
+    btnAuth.forEach((link) => {
+      link.addEventListener('click',(e) => {
+        e.preventDefault()
+        document.getElementById('modal-trigger').click()
+      })
+    })
   }
   toggleDropdownMenu() {
     const dropdownMenu = document.getElementById("menu-dropdown-items")
