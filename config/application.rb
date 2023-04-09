94c6d877c4b62cca236961a3ed412b06b56cc3ce
Thanhhiep <89078882+dth2k1@users.noreply.github.com>
2023-04-09 20:36:39 +0700

    2 add header and some gem (#1)

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module AirbnbClone
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:en, :vi, :ja]
   end
 end
