1512cdd8035342bdfccea0463ea47668b8149cd6
dth2k1 <gblhiep@gmail.com>
2023-04-17 00:35:15 +0700

    15 caculator distance user not login

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,11 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
+import { getDistance, convertSpeed } from 'geolib';
+var lat;
+var lon;
 export default class extends Controller {
+  
   connect() {
     window.navigator.geolocation.getCurrentPosition((pos)=> { 
-      var lat = pos.coords.latitude;
-      var lon = pos.coords.longitude;
+      lat = pos.coords.latitude;
+      lon = pos.coords.longitude;
       this.element.dataset.longitude = lon;
       this.element.dataset.latitude = lat;
     })
+    
+  }
+
+  get_distance() {
+    convertSpeed(getDistance(
+      {
+        latitude: lat,
+        longitude: lon
+      }
+      ,{
+        latitude: 51.525,
+        longitude: 7.4575,
+      }), 'km')
   }
 }
