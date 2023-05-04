276e1edfbccc58d9fdecc07a132b4d4efb8dab18
Thanhhiep <89078882+dth2k1@users.noreply.github.com>
2023-05-05 00:14:37 +0700

    20 add review model polymorphic (#14)

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :reviewable, polymorphic: true
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+end
