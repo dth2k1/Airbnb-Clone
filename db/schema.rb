d85a04fa43513a10a7d9d2541d4e444400a8abe5
dth2k1 <gblhiep@gmail.com>
2023-05-05 00:59:48 +0700

    21 add counter cache rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_153339) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_172909) do
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,6 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_153339) do
     t.float "latitude"
     t.integer "price_cents"
     t.string "price_currency"
+    t.integer "reviews_count"
+    t.decimal "average_rating", precision: 10
     t.index ["longitude", "latitude"], name: "index_properties_on_longitude_and_latitude"
   end
 
